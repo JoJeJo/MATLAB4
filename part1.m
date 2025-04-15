7fabd0cb75a8f232da9a620e62f55b80ace2f967
Jerom Jothiprakasam <109493932+JoJeJo@users.noreply.github.com>
2025-04-15 11:19:31 -0700

    Part 1

---
+++ part1.m
@@ -0,0 +1,14 @@
+%{
+Natalia Hopper
+Jerom Jothiprakasam
+Math 245 -- MATLAB Project 3
+(Part 1)
+
+Revision History
+Date            Changes             Programmer
+-------------------------------------------------------
+04/15/25        original            Jerom Jothiprakasam
+%}
+
+clc;
+clear;
